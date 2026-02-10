0da945eabf5893384cebdccaf447b9377d01e515
Baran Taşcı <barantasci44@gmail.com>
2026-02-10 21:23:22 +0300

    store and provider added

---
+++ src/app/providers.js
@@ -0,0 +1,13 @@
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../app/store";
+
+const Providers = ({ children }) => {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>{children}</BrowserRouter>
+        </Provider>
+    );
+};
+
+export default Providers;
