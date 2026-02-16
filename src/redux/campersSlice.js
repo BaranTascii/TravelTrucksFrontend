4702bc3b9ee1055f9f3dc3d02d946602447af654
Baran Taşcı <barantasci44@gmail.com>
2026-02-16 21:05:11 +0300

    campersSlice.js updated

---
+++ src/redux/campersSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers";
+
+export const fetchCampers = createAsyncThunk(
+  "campers/fetchAll",
+  async ({ page, filters }) => {
+    const params = new URLSearchParams({ page, limit: 4, ...filters });
+    const response = await axios.get(`${API_URL}?${params}`);
+    return response.data;
+  },
+);
+
+const campersSlice = createSlice({
+  name: "campers",
+  initialState: {
+    items: [],
+    favorites: JSON.parse(localStorage.getItem("favorites")) || [],
+    loading: false,
+    hasMore: true,
+  },
+  reducers: {
+    toggleFavorite: (state, action) => {
+      const id = action.payload;
+      state.favorites = state.favorites.includes(id)
+        ? state.favorites.filter((fav) => fav !== id)
+        : [...state.favorites, id];
+      localStorage.setItem("favorites", JSON.stringify(state.favorites));
+    },
+    resetItems: (state) => {
+      state.items = [];
+      state.hasMore = true;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCampers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchCampers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = [...state.items, ...action.payload];
+        if (action.payload.length < 4) state.hasMore = false;
+      });
+  },
+});
+
+export const { toggleFavorite, resetItems } = campersSlice.actions;
+export default campersSlice.reducer;
