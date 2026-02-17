d5afb1393978ce9583e6934721a47e9f9ce5a424
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 22:17:06 +0300

    Hero.jsx updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,19 @@
-import { useNavigate } from "react-router-dom";
 import styles from "./Hero.module.css";
-import hero from "../../assets/hero.png";
+import heroImg from "../../assets/images/hero.png";
+import { useNavigate } from "react-router-dom";
 
-export default function Hero() {
+function Hero() {
   const navigate = useNavigate();
 
   return (
-    <section className={styles.hero}>
-      <img src={hero} alt="hero" className={styles.image} />
-
-      <div className={styles.content}>
-        <h1>Travel Trucks</h1>
-        <p>Discover the freedom of van life</p>
-        <button className={styles.button} onClick={() => navigate("/catalog")}>
-          View Now
-        </button>
-      </div>
+    <section
+      className={styles.hero}
+      style={{ backgroundImage: `url(${heroImg})` }}
+    >
+      <h1>Travel with Comfort</h1>
+      <button onClick={() => navigate("/catalog")}>View Now</button>
     </section>
   );
 }
+
+export default Hero;
