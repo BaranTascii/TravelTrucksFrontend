9e283fa7aabe56bfe81b9fe5676b9eafa0ff7652
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 22:29:11 +0300

    Footer.jsx updated

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2026 TravelTrucks. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
