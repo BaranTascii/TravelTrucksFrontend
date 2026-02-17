a5f475e79032508226aeea010ad9df717389a26a
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 21:57:20 +0300

    HomePage updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,13 @@
-import { useNavigate } from "react-router-dom";
-import s from "./HomePage.module.css";
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
 
-const HomePage = () => {
-  const navigate = useNavigate();
+export default function HomePage() {
   return (
-    <section className={s.hero}>
-      <div className={`${s.heroContent} container`}>
-        <h1 className={s.title}>Campers of your dreams</h1>
-        <p className={s.subtitle}>
-          Explore our wide selection of campervans and start your adventure
-          today!
-        </p>
-        <button className={s.ctaBtn} onClick={() => navigate("/catalog")}>
-          View Now
-        </button>
-      </div>
-    </section>
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
   );
-};
-export default HomePage;
+}
