f00979d57cdf2c517ba8b4a2ec348b3c247a88d8
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 22:11:10 +0300

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
-import App from "./App.jsx";
-import "./index.css";
-import './styles/Root.css';
-
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./app/store";
+import "./styles/Root.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
