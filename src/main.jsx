6047a9d20944b2474c17be0dd5ffc1788c012ef9
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 21:55:53 +0300

    File structure reorganized

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App.jsx";
 import "./index.css";
+import './styles/Root.css';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
