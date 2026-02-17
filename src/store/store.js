da1ed729c7912f6339de009145cf4294374aaff6
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 21:14:44 +0300

    store.js updated

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import fleetReducer from "./fleetSlice";
-
-export const store = configureStore({
-  reducer: {
-    fleet: fleetReducer,
-  },
-});
