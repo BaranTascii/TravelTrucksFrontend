94be971f609e2c47752652292d967698933ee465
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 22:01:30 +0300

    CatalogPage updated

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,30 +1,33 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchCampers } from "../../store/fleetSlice";
+import { fetchCampers } from "../../redux/campers/campersOperations";
+import Header from "../../components/Header/Header";
 import CamperCard from "../../components/CamperCard/CamperCard";
-import s from "./CatalogPage.module.css";
+import FilterSidebar from "../../components/FilterSidebar/FilterSidebar";
+import Loader from "../../components/Loader/Loader";
+import styles from "./CatalogPage.module.css";
 
-const CatalogPage = () => {
+export default function CatalogPage() {
   const dispatch = useDispatch();
-  const { items, loading, error } = useSelector((state) => state.fleet);
+  const { items, isLoading, page } = useSelector((s) => s.campers);
 
   useEffect(() => {
-    dispatch(fetchCampers());
-  }, [dispatch]);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+    dispatch(fetchCampers({ page }));
+  }, [dispatch, page]);
 
   return (
-    <div className={`${s.layout} container`}>
-      <div className={s.list}>
-        {items && items.length > 0 ? (
-          items.map((camper) => <CamperCard key={camper.id} camper={camper} />)
-        ) : (
-          <p>No campers found. Please check your API connection.</p>
-        )}
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <FilterSidebar />
+        <div className={styles.list}>
+          {items.map((camper) => (
+            <CamperCard key={camper.id} camper={camper} />
+          ))}
+
+          {isLoading && <Loader />}
+        </div>
       </div>
-    </div>
+    </>
   );
-};
-export default CatalogPage;
+}
