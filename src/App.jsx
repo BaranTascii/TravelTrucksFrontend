2d25d626fe05ff0b435b0b42f7a37a354fe8cdbd
Baran Taşcı <barantasci44@gmail.com>
2026-02-17 22:36:56 +0300

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import CatalogPage from "./pages/CatalogPage/CatalogPage";
-import CamperDetailsPage from "./pages/CamperDetailsPage/CamperDetailsPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import AppRouter from "./components/AppRouter/AppRouter";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/catalog/:id" element={<CamperDetailsPage />} />
-      </Routes>
+      <AppRouter />
       <Footer />
     </>
   );
