146eaf88fec5403263f5ff6b0d76084f0c18896b
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:38:41 +0300

    Categories updated

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,56 @@
+import { CATEGORIES } from "../../constants/constants.js";
+import { capitalize } from "../../utils/utils.js";
+import style from "./Categories.module.css";
+
+const Categories = ({ camper }) => {
+  const transmission = camper.transmission;
+  const engine = camper.engine;
+  const categories = Object.keys(CATEGORIES).filter(
+    (category) => camper[category]
+  );
+
+  return (
+    <ul className={style.container}>
+      <li className={style.item}>
+        <svg
+          className={style.icon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+        >
+          <use href={"/icons.svg#transmission"}></use>
+        </svg>
+        <p>{capitalize(transmission)}</p>
+      </li>
+      <li className={style.item}>
+        <svg
+          className={style.icon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+        >
+          <use href={"/icons.svg#engine"}></use>
+        </svg>
+        <p>{capitalize(engine)}</p>
+      </li>
+      {categories.map((category, idx) => (
+        <li key={`categories-${idx}`} className={style.item}>
+          <svg
+            className={style.icon}
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+          >
+            <use href={`/icons.svg#${category}`}></use>
+          </svg>
+          <p>{CATEGORIES[category]}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Categories;
