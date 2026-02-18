38090e5c1aa07d63355be7887ad6441c281b9b3e
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:58:26 +0300

    Loader updated

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,23 @@
-import styles from "./Loader.module.css";
+import { ScaleLoader } from "react-spinners";
 
-export default function Loader() {
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
+const Loader = () => {
+  const color = "#E44848";
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.spinner}></div>
-    </div>
+    <ScaleLoader
+      color={color}
+      loading={true}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
   );
-}
+};
+
+export default Loader;
