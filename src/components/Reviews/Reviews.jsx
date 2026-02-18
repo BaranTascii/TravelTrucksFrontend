e7d6417d2ac5b86dacb0214c9d208652e72d017f
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 23:02:34 +0300

    Reviews updated

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,16 +1,47 @@
-import styles from "./Reviews.module.css";
+import { useOutletContext } from "react-router-dom";
+import style from "./Reviews.module.css";
 
-function Reviews() {
+const Reviews = () => {
+  const { reviews } = useOutletContext();
   return (
-    <div className={styles.reviews}>
-      <h3 className={styles.title}>Reviews</h3>
-
-      <div className={styles.review}>
-        <p className={styles.name}>John</p>
-        <p className={styles.text}>Amazing camper! Everything was perfect.</p>
-      </div>
+    <div className={style.container}>
+      {reviews.length > 0 ? (
+        <ul className={style.reviewsList}>
+          {reviews.map(({ reviewer_name, reviewer_rating, comment }, idx) => (
+            <li className={style.reviewItem} key={`review-${idx}`}>
+              <div className={style.header}>
+                <div className={style.icon}>{reviewer_name[0]}</div>
+                <div className={style.name}>
+                  <p>{reviewer_name}</p>
+                  <div className={style.stars}>
+                    {Array.from({ length: 5 }, (_, i) => (
+                      <svg
+                        key={`star-${i}`}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        className={
+                          i < reviewer_rating
+                            ? style.filledStar
+                            : style.emptyStar
+                        }
+                      >
+                        <use href="/icons.svg#star" />
+                      </svg>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <p className={style.comment}>{comment}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No reviews</p>
+      )}
     </div>
   );
-}
+};
 
 export default Reviews;
