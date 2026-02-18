1cf14400ea11b1d6582f3c0d27112f9082c459cc
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:08:43 +0300

    BookingForm updated

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,19 +1,100 @@
-import styles from "./BookingForm.module.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { object, string, date } from "yup";
+import style from "./BookingForm.module.css";
+import DateInput from "../DateInput/DateInput.jsx";
+import { toast } from "react-toastify";
 
-function BookingForm() {
+const BookingForm = ({ camperName }) => {
+  const initialValues = { name: "", email: "", bookingDate: "", comment: "" };
+  const BookingSchema = object({
+    name: string()
+      .min(3, "to short name")
+      .max(30, "to long name")
+      .required("required"),
+    email: string().email().required("required"),
+    bookingDate: date()
+      .required("required")
+      .min(new Date(), "Date must be in the future"),
+    comment: string().max(300, "to long comment"),
+  });
+
+  const handleSubmit = (values, action) => {
+    toast(
+      `Camper ${camperName} has been successfully booked for ${values.bookingDate.toDateString()}. Thank you for your reservation!`,
+    );
+    action.resetForm();
+  };
   return (
-    <div className={styles.card}>
-      <h3 className={styles.title}>Book your camper now</h3>
+    <section className={style.container}>
+      <div className={style.header}>
+        <h3 className={style.title}>Book your campervan now</h3>
+        <p className={style.desc}>
+          Stay connected! We are always ready to help you.
+        </p>
+      </div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={BookingSchema}
+        validateOnMount={true}
+        onSubmit={handleSubmit}
+      >
+        <Form className={style.form}>
+          <label className={style.label}>
+            <Field
+              className={style.input}
+              type="text"
+              name="name"
+              placeholder="Name*"
+            />
+            <ErrorMessage
+              name="name"
+              component="span"
+              className={style.error}
+            />
+          </label>
 
-      <input className={styles.input} placeholder="Name*" />
-      <input className={styles.input} placeholder="Email*" />
-      <input className={styles.input} type="date" />
+          <label className={style.label}>
+            <Field
+              className={style.input}
+              type="email"
+              name="email"
+              placeholder="Email*"
+            />
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={style.error}
+            />
+          </label>
 
-      <textarea className={styles.textarea} placeholder="Comment"></textarea>
-
-      <button className={styles.button}>Send</button>
-    </div>
+          <label className={style.label}>
+            <DateInput name="bookingDate" />
+            <ErrorMessage
+              name="bookingDate"
+              component="span"
+              className={style.error}
+            />
+          </label>
+          <label className={style.label}>
+            <Field
+              className={`${style.input} ${style.textarea}`}
+              as="textarea"
+              name="comment"
+              placeholder="Comment"
+            />
+            <ErrorMessage
+              name="comment"
+              component="span"
+              className={style.error}
+            />
+          </label>
+          <button className={style.sendBtn} type="submit">
+            Send
+          </button>
+        </Form>
+      </Formik>
+    </section>
   );
-}
+};
 
 export default BookingForm;
