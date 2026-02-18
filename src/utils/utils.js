52d1cc827ff6e5a6115556b9d4325dd5b73c3eec
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:04:02 +0300

    utils.js updated

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+export const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+export const addSpace = (string, lastChar = 1) => {
+  const length = string.length;
+  return (
+    string.slice(0, length - lastChar) + " " + string.slice(length - lastChar)
+  );
+};
