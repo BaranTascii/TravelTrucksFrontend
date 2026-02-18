6e7df5e24bd86b4051831739fee11f58d453c8ba
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:03:27 +0300

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { lazy, Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import AppRouter from "./routes/AppRouter";
+import Features from "./components/Features/Features";
+import Reviews from "./components/Reviews/Reviews";
+import Loader from "./components/Loader/Loader";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
+const CamperDetailPage = lazy(
+  () => import("./pages/CamperDetailPage/CamperDetailPage"),
+);
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Header />
-      <AppRouter />
-      <Footer />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/catalog/:id/" element={<CamperDetailPage />}>
+          <Route path="features" element={<Features />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="" element={<Features />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
