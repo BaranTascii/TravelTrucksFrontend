b7e525a99159afed07215d9852a760857bd50823
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:34:42 +0300

    CamperList updated

--- src/components/CampersList/CampersList.jsx
+++ src/components/CampersList/CampersList.jsx
@@ -1,16 +1,67 @@
-import CamperCard from "../CamperCard/CamperCard";
-import styles from "./CamperList.module.css";
+import { useSelector } from "react-redux";
+import {
+  selectCampers,
+  selectError,
+  selectIsLoading,
+  selectTotalCampers,
+} from "../../redux/campers/campersSlice.js";
+import Loader from "../Loader/Loader.jsx";
+import Camper from "../Camper/Camper.jsx";
+import style from "./CampersList.module.css";
+import { useState } from "react";
+import ImageModal from "../ImageModal/ImageModal.jsx";
 
-function CamperList() {
+const CampersList = ({ nextPage }) => {
+  const campers = useSelector(selectCampers);
+  const total = useSelector(selectTotalCampers);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedPhoto, setselectedPhoto] = useState(null);
+
+  const openModal = (photo) => {
+    setselectedPhoto(photo);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setselectedPhoto(null);
+  };
+
   return (
-    <div className={styles.list}>
-      <CamperCard />
-      <CamperCard />
-      <CamperCard />
-
-      <button className={styles.loadMore}>Load More</button>
+    <div className={style.container}>
+      {error && !error.includes("404") && (
+        <p className={style.message}>{error}</p>
+      )}
+      {campers.length > 0 ? (
+        <>
+          <ul className={style.camperList}>
+            {campers.map((camper) => (
+              <li key={camper.id} className={style.camperItem}>
+                <Camper camper={camper} openModal={openModal} />
+              </li>
+            ))}
+          </ul>
+          {campers.length < total && !isLoading && (
+            <button className={style.loadMoreBtn} onClick={() => nextPage()}>
+              Load more
+            </button>
+          )}
+        </>
+      ) : (
+        <>
+          {!isLoading && <p className={style.message}>No trucks available.</p>}
+        </>
+      )}
+      {isLoading && <Loader />}
+      <ImageModal
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+        selectedPhoto={selectedPhoto}
+      />
     </div>
   );
-}
+};
 
-export default CamperList;
+export default CampersList;
