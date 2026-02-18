07a67d96f535032fd3de3c5e86e02d1fa02dc0a1
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:05:54 +0300

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { store } from "./app/store";
-import "./styles/Root.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
-  </Provider>,
+  </StrictMode>
 );
