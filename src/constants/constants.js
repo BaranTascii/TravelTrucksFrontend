b831340176040ef303eadf73a1bdcf1a651ac2bb
Baran Taşcı <barantasci44@gmail.com>
2026-02-18 22:05:01 +0300

    constants.js updated

---
+++ src/constants/constants.js
@@ -0,0 +1,33 @@
+export const CATEGORIES = {
+  AC: "AC",
+  bathroom: "Bathroom",
+  kitchen: "Kitchen",
+  TV: "TV",
+  radio: "Radio",
+  refregirator: "Refregirator",
+  microwave: "Microwave",
+  gas: "Gas",
+  water: "Water",
+};
+
+export const FORM = {
+  panelTruck: "Panel Truck",
+  fullyIntegrated: "Fully Integrated",
+  alcove: "Alcove",
+};
+
+export const EQUIPMENTS = {
+  AC: "AC",
+  transmission: "Automatic",
+  kitchen: "Kitchen",
+  TV: "TV",
+  bathroom: "Bathroom",
+};
+
+export const TYPE = {
+  panelTruck: "Van",
+  fullyIntegrated: "Fully Integrated",
+  alcove: "Alcove",
+};
+
+export const PER_PAGE = 4;
